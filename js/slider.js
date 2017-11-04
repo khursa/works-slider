530b26e3583d882788e37aea486ff191549ea083
Khursa <khursa@protonmail.com>
2017-11-04 08:45:25 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.ws-slider').slick({
+  	infinite: true,
+ 	slidesToShow: 4,
+  	slidesToScroll: 2,
+  	arrows: true,
+  	autoplaySpeed: 10000,
+    centerMode: false
+  });
+});
